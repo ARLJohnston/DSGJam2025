ef5634bb2b7260c38223bfa799d0bef4d02d6789
interysy <2554268w@student.gla.ac.uk>
2025-02-28 22:19:09 +0000

    fix directional jump

--- player.gd
+++ player.gd
@@ -1,53 +1,42 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-
 var jump_charge = 0  
-const MAX_JUMP_CHARGE = -1000 
-const JUMP_GROWTH = 250
+const MAX_JUMP_CHARGE = -10000
+const JUMP_GROWTH = 2500
 var stretch_ratio = 1  
-var original_scale = 0
+var original_scale = Vector2()
 
 func _ready() -> void: 
 	original_scale = $DirectionArrow.scale
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor(): 
 		velocity += get_gravity() * delta 
-		if (velocity.x > 0):  
-			velocity.x -= 10 * delta
-		
+		velocity.x = move_toward(velocity.x, 0, 10 * delta)  
 
-	# Handle jump.
-	if Input.is_action_pressed("ui_accept") && is_on_floor() && jump_charge > MAX_JUMP_CHARGE:
+	if Input.is_action_pressed("ui_accept") and is_on_floor() and jump_charge > MAX_JUMP_CHARGE:
 		jump_charge -= JUMP_GROWTH * delta 
 		$DirectionArrow.visible = true  
 		$DirectionArrow.scale = original_scale
 		stretch_ratio = abs(jump_charge / MAX_JUMP_CHARGE)
-		$DirectionArrow.apply_scale(Vector2(stretch_ratio,1))      
-				   
-	
+		$DirectionArrow.apply_scale(Vector2(stretch_ratio, 1))
+
 	if Input.is_action_just_released("ui_accept"): 
 		var angle = $DirectionArrow.rotation 
-		var direction = Vector2(cos(angle),sin(angle))
-		self.velocity.y = direction.y * jump_charge * -1
-		self.velocity.x = direction.x * jump_charge * -1
+		var direction = Vector2(cos(angle), sin(angle)).normalized()
+		
+		velocity = direction * abs(jump_charge)
 
 		jump_charge = 0   
-		#$DirectionArrow.apply_scale(Vector2(-stretch_ratio,1))   
 		$DirectionArrow.scale = original_scale
 		stretch_ratio = 1 
 		$DirectionArrow.visible = false 
-	 
-	
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED 
+	var move_direction := Input.get_axis("ui_left", "ui_right")
+	if move_direction:
+		velocity.x = move_direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
