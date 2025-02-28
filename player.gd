a5f3dbd523afd521ccb500dcf7650bff8170b56e
ARLJohnston <github@arljohnston.com>
2025-02-28 19:43:48 +0000

    add charge jump

--- player.gd
+++ player.gd
@@ -4,15 +4,21 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var jump_charge = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_accept") && is_on_floor():
+		jump_charge -= 10
+	
+	if Input.is_action_just_released("ui_accept"):
+		velocity.y = jump_charge
+		jump_charge = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
