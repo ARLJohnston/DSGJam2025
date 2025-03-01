2ed2c26f4f017f5a41052eea2d59ff926b57801b
Knifa <knifa@users.noreply.github.com>
2025-03-01 17:02:26 +0000

    wip

--- arrow.gd
+++ arrow.gd
@@ -8,15 +8,11 @@ const MIN_ANGLE =  0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: 
 	self.visible = false  
-	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:    
-	
 	if (self.rotation > MAX_ANGLE || self.rotation < MIN_ANGLE):   
 		rotation_speed *= -1    
-	 
 	 
 	if (self.visible):  
 		self.rotation += delta * rotation_speed      
--- main.gd
+++ main.gd
@@ -1,33 +1,46 @@
 extends Node2D
 
 var player_spawn: Vector2;
-var ground_x_init: float;
-var landing_zone_x_init : float;
+var ground_spawn_x: float;
 var level = 1;
 
-# Called when the node enters the scene tree for the first time.
+var waiting_for_player_to_stop_after_ground_hit: bool = false;
+var waiting_for_post_win_timer: bool = false
+
 func _ready() -> void:
 	player_spawn = $Player/CharacterBody2D.position;
-	ground_x_init = $GapGround.position.x;  
-	landing_zone_x_init = $LandingZone.position.x; 
-	
-	
+	ground_spawn_x = $GapGround.position.x;  
+
 	$UDeadMate.connect("die", _on_die)
-	$GapGround/FinishLine.connect("win", _on_win)
+	$GapGround.connect("ground_touched", _on_ground_touched)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	# Player touched the ground and we're waiting for them to stop moving.
+	if waiting_for_player_to_stop_after_ground_hit and not waiting_for_post_win_timer:
+		var player_velocity: Vector2 = $Player/CharacterBody2D.velocity;
+		if player_velocity.length_squared() > 0.1:
+			return
+		
+		print("You landed!")
+		
+		# Wait a while to let the player mentally process their landing.
+		# TODO: Disable user inputs until after here.
+		get_tree().create_timer(2.0).connect("timeout", _on_win)
+		waiting_for_post_win_timer = true
+		
 func _on_win() -> void:
 	print("You win.")
 	
 	level = level + 1;
-	$GapGround.position.x = ground_x_init + 100 + (level ** 5) 
-	$LandingZone.has_scored = false 
-	$LandingZone.position.x = landing_zone_x_init + 100 + (level ** 5) 
+	$GapGround.position.x = ground_spawn_x + 100 + (level ** 5) 
+	$GapGround/LandingZone.has_scored = false 
 	_reset_player_pos()
+	
+	waiting_for_player_to_stop_after_ground_hit = false;
+	waiting_for_post_win_timer = false;
+
+func _on_ground_touched() -> void:
+	waiting_for_player_to_stop_after_ground_hit = true;
 	
 func _on_die() -> void:
 	print("You dead.")
