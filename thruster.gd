7a54751479b1a37453a46a1d292a114b267c22eb
interysy <2554268w@student.gla.ac.uk>
2025-03-01 21:45:59 +0000

    create thruster upgrade mechanism in the menu

---
+++ thruster.gd
@@ -0,0 +1,38 @@
+extends ReferenceRect
+ 
+
+var upgrade_level : int   
+const MAX_LEVEL = 3 
+var cost = [2000,4000,6000]
+
+enum MetalColor { BRONZE = 1, SILVER = 2, GOLD = 3 }
+
+# Dictionary to store corresponding colors
+var metal_colors = {
+	MetalColor.BRONZE: Color("#CD7F32"),  # Bronze
+	MetalColor.SILVER: Color("#C0C0C0"),  # Silver
+	MetalColor.GOLD: Color("#FFD700")     # Gold
+} 
+
+func _ready() -> void:
+	$BuyThrustUpgrade.text = "Buy upgrade " + str(cost[upgrade_level])
+
+func get_metal_color(value: int) -> Color:
+	return metal_colors.get(value, Color(1, 1, 1)) 
+
+func _on_buy_thrust_upgrade_pressed() -> void: 
+	if (upgrade_level != MAX_LEVEL):
+			$BuyThrustUpgrade.text = "Buy upgrade " + str(cost[upgrade_level]) 
+			
+	if (upgrade_level < MAX_LEVEL):
+		upgrade_level += 1 
+		self.border_color = get_metal_color(upgrade_level)    
+		match upgrade_level:
+			1:
+				$ThrustersUpgradeLevelStar1.modulate = get_metal_color(MetalColor.BRONZE)
+			2:
+				$ThrustersUpgradeLevelStar2.modulate = get_metal_color(MetalColor.SILVER)
+			3:
+				$ThrustersUpgradeLevelStar3.modulate = get_metal_color(MetalColor.GOLD)
+			_:
+				print("Invalid level")
