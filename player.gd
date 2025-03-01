a61205bd36b75bd1b6e14c7f6f48fd0c6e753cfb
interysy <2554268w@student.gla.ac.uk>
2025-03-01 10:54:01 +0000

    add smooth directional jump and avoid moving in the air

--- player.gd
+++ player.gd
@@ -27,12 +27,13 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_released("ui_accept") and is_on_floor():
 		jump()
-	
-	var move_direction := Input.get_axis("ui_left", "ui_right")
-	if move_direction:
-		velocity.x = move_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	 
+	if (is_on_floor()):
+		var move_direction := Input.get_axis("ui_left", "ui_right") 
+		if move_direction:
+			velocity.x = move_direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
