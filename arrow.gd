57d6ba40f5cbcce043bad73784fc9a8eaca1cd41
interysy <2554268w@student.gla.ac.uk>
2025-03-01 10:39:10 +0000

    use patrick's arrow as indicator of jump direction

--- arrow.gd
+++ arrow.gd
@@ -1,15 +1,13 @@
 extends Sprite2D
 
-var rotation_speed = - 0.25
-const MAX_ANGLE = 0
-const MIN_ANGLE =  - (PI / 2)
-var current_angle = 0 
-
+var rotation_speed = - 3
+const MAX_ANGLE = (PI / 2) 
+const MIN_ANGLE =  0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void: 
-	self.visible = false 
+	self.visible = false  
 	pass
 
 
@@ -18,8 +16,9 @@ func _process(delta: float) -> void:
 	
 	if (self.rotation > MAX_ANGLE || self.rotation < MIN_ANGLE):   
 		rotation_speed *= -1    
-	
-	if (self.visible): 
-		self.rotation += delta * rotation_speed     
+	 
+	 
+	if (self.visible):  
+		self.rotation += delta * rotation_speed      
 		
 	
--- player.gd
+++ player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 800.0
 var jump_charge = 0
-const MAX_JUMP_CHARGE = -10000
-const JUMP_GROWTH = 2500
+const MAX_JUMP_CHARGE = -5000
+const JUMP_GROWTH = 1250
 var stretch_ratio = 1
 var original_scale = Vector2()
+var gravity = 980.0
+var is_jumping = false
 
 func _ready() -> void:
 	original_scale = $DirectionArrow.scale
@@ -13,27 +15,19 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 		velocity.x = move_toward(velocity.x, 0, 10 * delta)
-
+	
 	if Input.is_action_pressed("ui_accept") and is_on_floor() and jump_charge > MAX_JUMP_CHARGE:
 		jump_charge -= JUMP_GROWTH * delta
 		$DirectionArrow.visible = true
 		$DirectionArrow.scale = original_scale
 		stretch_ratio = abs(jump_charge / MAX_JUMP_CHARGE)
-		$DirectionArrow.apply_scale(Vector2(stretch_ratio, 1))
-
-	if Input.is_action_just_released("ui_accept"):
-		var angle = $DirectionArrow.rotation
-		var direction = Vector2(cos(angle), sin(angle)).normalized()
-
-		velocity = direction * abs(jump_charge)
-
-		jump_charge = 0
-		$DirectionArrow.scale = original_scale
-		stretch_ratio = 1
-		$DirectionArrow.visible = false
-
+		$DirectionArrow.apply_scale(Vector2(1, stretch_ratio))
+	
+	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		jump()
+	
 	var move_direction := Input.get_axis("ui_left", "ui_right")
 	if move_direction:
 		velocity.x = move_direction * SPEED
@@ -41,3 +35,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func jump():
+	is_jumping = true
+	var angle = $DirectionArrow.rotation
+	var direction = Vector2(cos(angle), sin(angle)).normalized()
+	velocity = Vector2(0.0 , jump_charge).rotated(angle)
+
+	jump_charge = 0
+	$DirectionArrow.scale = original_scale
+	stretch_ratio = 1
+	$DirectionArrow.visible = false
+
+	await get_tree().create_timer(0.5).timeout
+	is_jumping = false
