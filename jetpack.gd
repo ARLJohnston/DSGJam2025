2a61797bb58249b1286d61a29e9f87edd0d12910
interysy <2554268w@student.gla.ac.uk>
2025-03-02 10:19:20 +0000

    remove unnecessary jetpack code

--- jetpack.gd
+++ jetpack.gd
@@ -16,9 +16,7 @@ var metal_colors = {
 
 func _ready() -> void:
 	$BuyJetpackUpgrade.text = "Buy upgrade " + str(cost[upgrade_level]) 
-	$JetpackUpgradeLevelStar1.material.set_shader_parameter("shine_enabled", false)   
-	$JetpackUpgradeLevelStar2.material.set_shader_parameter("shine_enabled", false) 
-	$JetpackUpgradeLevelStar3.material.set_shader_parameter("shine_enabled", false)
+
 
 func get_metal_color(value: int) -> Color:
 	return metal_colors.get(value, Color(1, 1, 1)) 
@@ -36,12 +34,9 @@ func _on_buy_jetpack_upgrade_pressed() -> void:
 		match upgrade_level:
 			1:
 				$JetpackUpgradeLevelStar1.modulate = get_metal_color(MetalColor.BRONZE) 
-				$JetpackUpgradeLevelStar1.material.set_shader_parameter("shine_enabled", true) 
 			2:
 				$JetpackUpgradeLevelStar2.modulate = get_metal_color(MetalColor.SILVER) 
-				$JetpackUpgradeLevelStar2.material.set_shader_parameter("shine_enabled", true)
 			3:
 				$JetpackUpgradeLevelStar3.modulate = get_metal_color(MetalColor.GOLD) 
-				$JetpackUpgradeLevelStar3.material.set_shader_parameter("shine_enabled", true)
 			_:
 				print("Invalid level")
