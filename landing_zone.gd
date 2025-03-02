08939eb18f6f4cf21eabef4c757305cdaf9d9ded
interysy <2554268w@student.gla.ac.uk>
2025-03-02 11:59:15 +0000

    add multiplier based on landing zone

--- landing_zone.gd
+++ landing_zone.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-signal land
+signal land  
+
 
 @onready var landing_zone = $LandingZone
 @onready var triple_zone = $LandingZone/TripleZone
 @onready var double_zone = $LandingZone/DoubleZone
 @onready var single_zone = $LandingZone/SingleZone
 
-var has_scored = false
+var has_scored = false 
+
+var bullseye = false; 
+var fivex = false; 
+var twox = false;
 
 func _ready() -> void:
 	pass
@@ -16,16 +21,16 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if not has_scored:
 		print("Landed in Triple Zone! +300 points")
 		has_scored = true
-		land.emit()
+		bullseye = true
 
 func _on_single_body_entered(body: Node2D) -> void:
 	if not has_scored:
 		print("Landed in Single Zone! +100 points")
 		has_scored = true
-		land.emit()
+		fivex = true 
 
 func _on_double_body_entered(body: Node2D) -> void:
 	if not has_scored:
 		print("Landed in Double Zone! +200 points")
 		has_scored = true
-		land.emit()
+		twox = true
--- main.gd
+++ main.gd
@@ -6,7 +6,8 @@ var ground_spawn_x: float;
 var level = 1;
 var gap = 0;
 var distance = 0;
-var isk = 0;
+var isk = 0; 
+var last_isk_added = 0; 
 
 var start_line_spawn_y: float;
 var finish_line_spawn_y: float;
@@ -47,7 +48,7 @@ func _ready() -> void:
 	$ShopUI/ColorRect/MaxJumpPower.isk_updated.connect(_on_isk_updated) 
 	$ShopUI/ColorRect/XJump.isk_updated.connect(_on_isk_updated) 
 	$ShopUI/ColorRect/Thrusters.isk_updated.connect(_on_isk_updated) 
-	$ShopUI/ColorRect/Jetpack.isk_updated.connect(_on_isk_updated)
+	$ShopUI/ColorRect/Jetpack.isk_updated.connect(_on_isk_updated) 
 	
 	
 	_reset()
@@ -91,8 +92,22 @@ func _on_win() -> void:
 		level = level + 1;
 		next_gap = 100 + (level ** 4);
 	$GapGround.position.x = ground_spawn_x + next_gap;
+	 
+	var landing_zone = $GapGround/LandingZone  
+	var reward_isk = round(distance / 100 * 1.2)
 	
-	_inc_isk(round(distance / 100 * 1.2));
+	if ($GapGround/LandingZone.bullseye): 
+		_inc_isk(reward_isk * 10, 10) 
+		$GapGround/LandingZone.bullseye = false 
+	elif ($GapGround/LandingZone.fivex): 
+		_inc_isk(reward_isk * 5, 5)         
+		$GapGround/LandingZone.fivex = false  
+	elif ($GapGround/LandingZone.twox): 
+		_inc_isk(reward_isk * 2, 2) 
+		$GapGround/LandingZone.twox = false   
+	else: 
+		_inc_isk(reward_isk) 
+	
 	
 	$CanvasLayer/DistanceLabel.modulate = Color.GREEN;
 	
@@ -144,12 +159,27 @@ func _reset() -> void:
 	waiting_for_player_to_stop_after_ground_hit = false;
 	waiting_for_post_win_timer = false;
 	started = false
-
-func _inc_isk(value: float) -> void:
-	var isk_inc = round(distance / 100 * 1.2);
-	isk += isk_inc;
+ 
+func _multiply_process(isk_inc : int , multiplier : int): 
 	
-	$CanvasLayer/IskLabel/IskPlusLabel.animate(isk_inc);
+	$CanvasLayer/IskLabel/IskPlusLabel.animate_multiplier(isk_inc, multiplier)
+	isk = isk - isk_inc + (isk_inc * multiplier) 
+	$CanvasLayer/IskLabel.text = "§%d ISK" % isk; 
+	$ShopUI/ColorRect/IskLabel.text = "§%d ISK" % isk;  
+	
+func _inc_isk(value: float, multiplier : int = 0) -> void:
+	var isk_inc = round(distance / 100 * 1.2);
+	isk += isk_inc; 
+	$CanvasLayer/IskLabel.text = "§%d ISK" % isk; 
+	$ShopUI/ColorRect/IskLabel.text = "§%d ISK" % isk; 
+	 
+	if (multiplier != 0): 
+		$CanvasLayer/IskLabel/IskPlusLabel.animate(isk_inc);  
+		get_tree().create_timer(2.0).timeout.connect(Callable(self, "_multiply_process").bind(isk_inc, multiplier))
+
+
+	else:
+		$CanvasLayer/IskLabel/IskPlusLabel.animate(isk_inc);
 	
 func _pin_line_marker_y(line: Sprite2D, spawn_y: float):
 	"""Pin a line marker (start/end) to the player sprite so that it can
@@ -169,4 +199,6 @@ func _on_isk_updated(amount):
 	isk = amount  
 	$CanvasLayer/IskLabel.text = "§%d ISK" % isk; 
 	$ShopUI/ColorRect/IskLabel.text = "§%d ISK" % isk; 
-	print("isk updated")
+
+
+	
